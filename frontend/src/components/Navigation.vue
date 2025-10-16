c18bdd16f66ebe5920e9c02f9f3b8a21dbb1581d
Sam9610 <pascolosamuele@gmail.com>
2025-10-16 15:32:05 +0200

    regole responsive

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -3,7 +3,10 @@
     <router-link :to="`/game/${gameStore.gameId}/production`" :class="linkClass">{{ $t('navigation.buttons.production') }}</router-link>
     <router-link :to="`/game/${gameStore.gameId}/sales`" :class="linkClass">{{ $t('navigation.buttons.sales') }}</router-link>
     <router-link :to="`/game/${gameStore.gameId}/hr`" :class="linkClass">{{ $t('navigation.buttons.hr') }}</router-link>
-    <router-link to="/" class="exit-link" :class="[linkClass, 'exit-link']">{{ $t('navigation.buttons.exit') }}</router-link>
+    <router-link to="/" class="exit-link" :class="[linkClass, 'exit-link']">
+      <span class="long-text">{{ $t('navigation.buttons.exit') }}</span>
+      <span class="short-text">{{ $t('navigation.buttons.exit_short') }}</span>
+    </router-link>
   </nav>
 </template>
   
